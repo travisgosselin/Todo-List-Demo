2af4619685f6f8b2194a3cc8e1961951e8968ce7
Travis Gosselin <travis.gosselin@live.ca>
2016-01-25 21:36:46 -0500

    Addition of working angular template in bootstrap 4.

--- src/TodoListDemo.Web/Assets/app/todo.controller.js
+++ src/TodoListDemo.Web/Assets/app/todo.controller.js
@@ -1,4 +1,18 @@
 ﻿angular.module('todo')
 	.controller('TodoController', ['$scope', '$routeParams', '$filter', function($scope, $routeParams, $filter) {
-        $scope.test = 'hello2';
+        
+        $scope.addTodo = function(todoText) {
+            $scope.todos.push({ text: todoText, isComplete: false });
+            $scope.newTodo = '';
+        }
+
+        $scope.completeTodo = function(todo) {
+            todo.isComplete = !todo.isComplete;
+        };
+
+        $scope.deleteTodo = function(todo) {
+            var index = $scope.todos.indexOf(todo);
+            $scope.todos.splice(index, 1);
+        };
+
     }]);
